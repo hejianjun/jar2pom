b33b637e876846ec8a43bbfc5b8e7fa3edd2a80d
gaojunru <gaojunru@thunisoft.com>
2019-06-11 11:14:31 +0800

    修改sonar问题

--- src/main/java/com/jxh/tool/Jar2pom.java
+++ src/main/java/com/jxh/tool/Jar2pom.java
@@ -1,5 +1,7 @@
 package com.jxh.tool;
 
+import lombok.extern.apachecommons.CommonsLog;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -16,7 +18,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
-import java.util.jar.*;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 /**
  * 通过Jar包SHA1或MD5生成Pom文件
@@ -24,108 +28,119 @@ import java.util.jar.*;
  * @author JiaXiaohei
  * @date 2018-03-16
  */
+@CommonsLog
 public class Jar2pom {
 
     /**
-     * Maven库
+     * maven库查询地址
      */
-    public static final String nexusUrl = "http://59.110.144.164/nexus/service/local/lucene/search";
+    private static final String NEXUS_URL = "http://repo.thunisoft.com/maven2/service/local/lucene/search";
+
+    /**
+     * jar地址
+     */
+    private static final String LIB_PATH = "E:\\project\\广西高院审委会项目\\shxt\\web\\WEB-INF\\lib";
 
     public static void main(String[] args) {
-        //先通过Jar的SHA1查询 如果不存在则解析Manifest查询
-        File libs = new File("D:\\lib");
-        for (File jar : libs.listFiles()) {
-            System.out.println("<!--  " + jar.getName() + " -->");
-            if (!getPomByChecksum(jar).isTextOnly()) {
-                System.out.println("<!--  Search by Checksum -->");
-                System.out.println(getPomByChecksum(jar).asXML());
-            } else if (!getPomByManifest(jar).isTextOnly()) {
-                System.out.println("<!--  Search by Manifest -->");
-                System.out.println(getPomByManifest(jar).asXML());
-            } else {
-                System.out.println("<!--  No data was found -->");
-            }
-            System.out.println();
+        StringBuilder builder = new StringBuilder();
+        // 先通过Jar的SHA1查询 如果不存在则解析Manifest查询
+        File libPath = new File(LIB_PATH);
+        File[] libs = libPath.listFiles();
+        if (libs == null) {
+            return;
         }
-
+        for (File jar : libs) {
+            builder.append("<!--  ").append(jar.getName()).append(" -->\n");
+            if (!getPomByChecksum(jar).isTextOnly()) {
+                builder.append("<!--  Search by Checksum -->\n");
+                builder.append(getPomByChecksum(jar).asXML());
+            } else if (!getPomByManifest(jar).isTextOnly()) {
+                builder.append("<!--  Search by Manifest -->\n");
+                builder.append(getPomByManifest(jar).asXML());
+            } else {
+                builder.append("<!--  No data was found -->");
+            }
+            builder.append("\n");
+        }
+        log.info("pom信息：\n" + builder);
     }
 
     /**
      * 通过Jar SHA1返回Pom dependency
      *
-     * @param file
-     * @return
+     * @param file jar文件
+     * @return xml元素
      */
-    public static Element getPomByChecksum(File file) {
+    private static Element getPomByChecksum(File file) {
         String checkSum = getCheckSum(file, "SHA1");
-        String xml = doGet(nexusUrl + "?sha1=" + checkSum);
+        String xml = doGet(NEXUS_URL + "?sha1=" + checkSum);
         return assemblePomElement(xml);
     }
 
     /**
      * 通过Jar Manifest返回Pom dependency
      *
-     * @param file
-     * @return
+     * @param file jar文件
+     * @return xml元素
      */
-    public static Element getPomByManifest(File file) {
-        try {
-            JarFile jarfile = new JarFile(file);
-            Manifest mainmanifest = jarfile.getManifest();
-            jarfile.close();
-            if (null == mainmanifest) {
-                return new DOMElement("dependency");
+    private static Element getPomByManifest(File file) {
+        try (JarFile jarfile = new JarFile(file)) {
+            Manifest manifest = jarfile.getManifest();
+            if (null == manifest) {
+                return createDependency();
             }
-            String a = null, v = null;
-            if (mainmanifest.getMainAttributes().containsKey(new Attributes.Name("Extension-Name"))) {
-                a = mainmanifest.getMainAttributes().getValue(new Attributes.Name("Extension-Name"));
-            } else if (mainmanifest.getMainAttributes().containsKey(new Attributes.Name("Implementation-Title"))) {
-                a = mainmanifest.getMainAttributes().getValue(new Attributes.Name("Implementation-Title"));
-            } else if (mainmanifest.getMainAttributes().containsKey(new Attributes.Name("Specification-Title"))) {
-                a = mainmanifest.getMainAttributes().getValue(new Attributes.Name("Specification-Title"));
+            String a = null;
+            if (manifest.getMainAttributes().containsKey(new Attributes.Name("Extension-Name"))) {
+                a = manifest.getMainAttributes().getValue(new Attributes.Name("Extension-Name"));
+            } else if (manifest.getMainAttributes().containsKey(new Attributes.Name("Implementation-Title"))) {
+                a = manifest.getMainAttributes().getValue(new Attributes.Name("Implementation-Title"));
+            } else if (manifest.getMainAttributes().containsKey(new Attributes.Name("Specification-Title"))) {
+                a = manifest.getMainAttributes().getValue(new Attributes.Name("Specification-Title"));
             }
-            if (a != null && a.length() != 0) {
+            if (StringUtils.isNotBlank(a)) {
                 a = a.replace("\"", "").replace(" ", "-");
             }
-            if (mainmanifest.getMainAttributes().containsKey(new Attributes.Name("Bundle-Version"))) {
-                v = mainmanifest.getMainAttributes().getValue(new Attributes.Name("Bundle-Version"));
-            } else if (mainmanifest.getMainAttributes().containsKey(new Attributes.Name("Implementation-Version"))) {
-                v = mainmanifest.getMainAttributes().getValue(new Attributes.Name("Implementation-Version"));
-            } else if (mainmanifest.getMainAttributes().containsKey(new Attributes.Name("Specification-Version"))) {
-                v = mainmanifest.getMainAttributes().getValue(new Attributes.Name("Specification-Version"));
+            String v = null;
+            if (manifest.getMainAttributes().containsKey(new Attributes.Name("Bundle-Version"))) {
+                v = manifest.getMainAttributes().getValue(new Attributes.Name("Bundle-Version"));
+            } else if (manifest.getMainAttributes().containsKey(new Attributes.Name("Implementation-Version"))) {
+                v = manifest.getMainAttributes().getValue(new Attributes.Name("Implementation-Version"));
+            } else if (manifest.getMainAttributes().containsKey(new Attributes.Name("Specification-Version"))) {
+                v = manifest.getMainAttributes().getValue(new Attributes.Name("Specification-Version"));
             }
-            if (v != null && v.length() != 0) {
+            if (StringUtils.isNotBlank(v)) {
                 v = v.replace("\"", "").replace(" ", "-");
             }
-            String xml = doGet(nexusUrl + "?a=" + a + "&v=" + v);
+            String xml = doGet(NEXUS_URL + "?a=" + a + "&v=" + v);
             return assemblePomElement(xml);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error(e);
         }
-        return new DOMElement("dependency");
+        return createDependency();
     }
 
     /**
      * 解析获取的XML 组装dependency
      *
-     * @param xml
-     * @return
+     * @param xml 字符串
+     * @return xml元素
      */
-    public static Element assemblePomElement(String xml) {
-        Element dependency = new DOMElement("dependency");
+    private static Element assemblePomElement(String xml) {
+        Element dependency = createDependency();
 
-        if (xml != null && xml.length() != 0) {
+        if (StringUtils.isNotBlank(xml)) {
             try {
                 Document document = DocumentHelper.parseText(xml);
                 Element dataElement = document.getRootElement().element("data");
-                if (dataElement.getText() != null && dataElement.getText().length() != 0) {
+                String text = dataElement.getText();
+                if (StringUtils.isNotBlank(text)) {
                     Element artifactElement = dataElement.element("artifact");
                     dependency.add((Element) artifactElement.element("groupId").clone());
                     dependency.add((Element) artifactElement.element("artifactId").clone());
                     dependency.add((Element) artifactElement.element("version").clone());
                 }
             } catch (DocumentException e) {
-                e.printStackTrace();
+                log.error(e);
             }
         }
         return dependency;
@@ -134,56 +149,53 @@ public class Jar2pom {
     /**
      * 发起Get请求
      *
-     * @param url
-     * @return
+     * @param url 请求地址
+     * @return 返回结果
      */
-    public static String doGet(String url) {
-        String srtResult = null;
-        CloseableHttpClient httpCilent = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(url);
-        try {
-            HttpResponse httpResponse = httpCilent.execute(httpGet);
-            srtResult = EntityUtils.toString(httpResponse.getEntity());
+    private static String doGet(String url) {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet(url);
+            HttpResponse httpResponse = httpClient.execute(httpGet);
+            return EntityUtils.toString(httpResponse.getEntity());
         } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                httpCilent.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            log.error(e);
         }
-        return srtResult;
+        return null;
     }
 
     /**
      * 计算CheckSum
      *
-     * @param file
+     * @param file      文件
      * @param algorithm SHA1 or MD5
-     * @return
+     * @return CheckSum
      */
-    public static String getCheckSum(File file, String algorithm) {
+    private static String getCheckSum(File file, String algorithm) {
         if (!file.isFile()) {
             return null;
         }
-        MessageDigest digest = null;
-        FileInputStream in = null;
-        byte buffer[] = new byte[1024];
-        int len;
-        try {
-            digest = MessageDigest.getInstance(algorithm);
-            in = new FileInputStream(file);
+        try (FileInputStream in = new FileInputStream(file)) {
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+            byte[] buffer = new byte[1024];
+            int len;
             while ((len = in.read(buffer, 0, 1024)) != -1) {
                 digest.update(buffer, 0, len);
             }
-            in.close();
+            BigInteger bigInt = new BigInteger(1, digest.digest());
+            return bigInt.toString(16);
         } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+            log.error(e);
         }
-        BigInteger bigInt = new BigInteger(1, digest.digest());
-        return bigInt.toString(16);
+        return null;
+    }
+
+    /**
+     * 创建xml节点
+     *
+     * @return xml节点
+     */
+    private static DOMElement createDependency() {
+        return new DOMElement("dependency");
     }
 
 }
